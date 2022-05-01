4d50f91b709d699067ea5bbb7cfb764a715d22a2
Juliana Oliveira <h4bituei.o@gmail.com>
2022-05-01 17:46:27 -0300

    Utilizando recurso @Getter e @Setter da Lombok na classe Address.java

--- src/main/java/org/agoncal/application/petstore/domain/Address.java
+++ src/main/java/org/agoncal/application/petstore/domain/Address.java
@@ -4,6 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author Antonio Goncalves
@@ -17,7 +19,8 @@ public class Address {
     // ======================================
     // =             Attributes             =
     // ======================================
-
+    @Getter
+    @Setter
     @Column(nullable = false)
     @NotNull
     @Size(min = 5, max = 50)
@@ -50,59 +53,6 @@ public class Address {
         this.zipcode = zipcode;
         this.country = country;
     }
-
-    // ======================================
-    // =         Getters & setters          =
-    // ======================================
-
-    public String getStreet1() {
-        return street1;
-    }
-
-    public void setStreet1(String street1) {
-        this.street1 = street1;
-    }
-
-    public String getStreet2() {
-        return street2;
-    }
-
-    public void setStreet2(String street2) {
-        this.street2 = street2;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
     // ======================================
     // =   Methods hash, equals, toString   =
     // ======================================
