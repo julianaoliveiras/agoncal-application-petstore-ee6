92aaa0f57ad9972b339f8ccafe47866618a1bb97
Juliana Oliveira <h4bituei.o@gmail.com>
2022-05-01 17:49:41 -0300

    Utilizando recurso @Getter e @Setter da Lombok na classe CartItem.java

--- src/main/java/org/agoncal/application/petstore/domain/CartItem.java
+++ src/main/java/org/agoncal/application/petstore/domain/CartItem.java
@@ -2,6 +2,8 @@ package org.agoncal.application.petstore.domain;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author Antonio Goncalves
@@ -14,7 +16,8 @@ public class CartItem {
     // ======================================
     // =             Attributes             =
     // ======================================
-
+    @Getter
+    @Setter
     @NotNull
     private Item item;
     @NotNull
@@ -37,27 +40,7 @@ public class CartItem {
     public Float getSubTotal() {
         return item.getUnitCost() * quantity;
     }
-
-    // ======================================
-    // =         Getters & setters          =
-    // ======================================
-
-    public Item getItem() {
-        return item;
-    }
-
-    public void setItem(Item item) {
-        this.item = item;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
+    
     // ======================================
     // =   Methods hash, equals, toString   =
     // ======================================
